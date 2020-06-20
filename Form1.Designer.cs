9a9780b7a4227d442afc4f8e29e829425c3f2cea
MochiJammy <ngh2@unlv.nevada.edu>
2020-06-19 20:05:12 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SoundProjectTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.Record = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.sourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Display = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.sigChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.CreateWindow = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sigChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(580, 3);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(180, 37);
+            this.Refresh.TabIndex = 0;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Record
+            // 
+            this.Record.Location = new System.Drawing.Point(12, 3);
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(180, 37);
+            this.Record.TabIndex = 1;
+            this.Record.Text = "Record";
+            this.Record.UseVisualStyleBackColor = true;
+            this.Record.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(12, 46);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(180, 37);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // sourceList
+            // 
+            this.sourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sourceList.HideSelection = false;
+            this.sourceList.Location = new System.Drawing.Point(580, 46);
+            this.sourceList.Name = "sourceList";
+            this.sourceList.Size = new System.Drawing.Size(366, 102);
+            this.sourceList.TabIndex = 3;
+            this.sourceList.UseCompatibleStateImageBehavior = false;
+            this.sourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Source";
+            this.columnHeader2.Width = 176;
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(198, 46);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(180, 37);
+            this.Display.TabIndex = 4;
+            this.Display.Text = "Display";
+            this.Display.UseVisualStyleBackColor = true;
+            this.Display.Click += new System.EventHandler(this.Display_Click);
+            // 
+            // sigChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.sigChart.ChartAreas.Add(chartArea1);
+            this.sigChart.Location = new System.Drawing.Point(12, 154);
+            this.sigChart.Name = "sigChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.sigChart.Series.Add(series1);
+            this.sigChart.Size = new System.Drawing.Size(934, 407);
+            this.sigChart.TabIndex = 5;
+            this.sigChart.Text = "chart1";
+            // 
+            // CreateWindow
+            // 
+            this.CreateWindow.Location = new System.Drawing.Point(198, 3);
+            this.CreateWindow.Name = "CreateWindow";
+            this.CreateWindow.Size = new System.Drawing.Size(180, 37);
+            this.CreateWindow.TabIndex = 6;
+            this.CreateWindow.Text = "Next Window";
+            this.CreateWindow.UseVisualStyleBackColor = true;
+            this.CreateWindow.Click += new System.EventHandler(this.CreateWindow_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(766, 3);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(180, 37);
+            this.Exit.TabIndex = 7;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(12, 89);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(180, 37);
+            this.Play.TabIndex = 8;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 573);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.CreateWindow);
+            this.Controls.Add(this.sigChart);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.sourceList);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Record);
+            this.Controls.Add(this.Refresh);
+            this.Name = "Form1";
+            this.Text = "Signal Project";
+            ((System.ComponentModel.ISupportInitialize)(this.sigChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Button Record;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.ListView sourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button Display;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart sigChart;
+        private System.Windows.Forms.Button CreateWindow;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Play;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Media;
+
+namespace SoundProjectTwo
+{
+    public partial class Form1 : Form
+    {
+        private NAudio.Wave.WaveIn sourceStream = null;
+        private NAudio.Wave.WaveFileWriter waveWriter = null;
+        private NAudio.Wave.DirectSoundOut waveOut = null;
+
+        private System.IO.MemoryStream ms = new System.IO.MemoryStream();
+        private int sampleRate = 16000;
+        private int window = 0;
+        private float[] signal = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+            for(int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
+            sourceList.Items.Clear();
+            foreach(var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                sourceList.Items.Add(item);
+            }
+        }
+
+        private void Record_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int device = sourceList.SelectedItems[0].Index;
+                sourceStream = new NAudio.Wave.WaveIn();
+                sourceStream.DeviceNumber = device;
+                sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(sampleRate, NAudio.Wave.WaveIn.GetCapabilities(device).Channels);
+                sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+                waveWriter = new NAudio.Wave.WaveFileWriter(ms, sourceStream.WaveFormat);
+                sourceStream.StartRecording();
+            }
+            catch
+            {
+                MessageBox.Show("Please Select An Audio Device", "Missing Audio Device", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if(waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if(sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+
+            if(waveWriter != null)
+            {
+                waveWriter.Dispose();
+                waveWriter = null;
+            }
+        }
+
+        private void Display_Click(object sender, EventArgs e)
+        {
+            byte[] allByte = ms.ToArray();
+            signal = new float[allByte.Length / 2];
+
+            for (int i = 0; i < allByte.Length; i += 2)
+            {
+                short sample = (short)((allByte[i + 1] << 8) | allByte[i + 0]);
+                float sample32 = sample / 32768f;
+                signal[i / 2] = sample32;
+            }
+
+            sigChart.Series["Series1"].Points.Clear();
+            ChartArea CA = sigChart.ChartAreas[0];
+            CA.AxisX.ScaleView.Zoomable = true;
+            CA.CursorX.AutoScroll = true;
+            CA.CursorX.IsUserSelectionEnabled = true;
+            for(int i = 0; i < signal.Length; i++)
+            {
+                if(i % 400 == 0)
+                {
+                    sigChart.Series["Series1"].Points.AddY(signal[i]);
+                }
+            }
+        }
+
+        private double[] Apply_Hamming_Code(double[] signalR)
+        {
+            double[] signalW = new double[signalR.Length];
+            double[] signalQ = new double[signalR.Length];
+
+            for(int i = 0; i < signalR.Length; i++)
+            {
+                signalW[i] = 0.54 - 0.46 * Math.Cos(2 * (Math.PI) * i / 399);
+            }
+
+            for(int i = 0; i < signalR.Length; i++)
+            {
+                signalQ[i] = signalR[i] * signalW[i];
+            }
+
+            return signalQ;
+        }
+
+        private double[] Apply_FFT(double[] signalQ)
+        {
+            double[] signalU = new double[signalQ.Length];
+            double[] signalV = new double[signalQ.Length];
+            double[] spectral_density = new double[signalQ.Length];
+
+            for(int i = 0; i < 400; i++)
+            {
+                double U = 0;
+                double V = 0;
+
+                for(int j = 0; j < 400; j++)
+                {
+                    U += Math.Cos(2 * Math.PI * i * j / 400) * signalQ[j];
+                    V += Math.Sin(2 * Math.PI * i * j / 400);
+                }
+
+                signalU[i] = U;
+                signalV[i] = V;
+                spectral_density[i] = Math.Pow(U, 2) + Math.Pow(V, 2);
+            }
+
+            return spectral_density;
+        }
+
+        private void CreateWindow_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form2();
+            double[] signalR = new double[400];
+            for(int i = 0; i < signalR.Length; i++)
+            {
+                signalR[i] = signal[i + this.window];
+            }
+            double[] signalQ = Apply_Hamming_Code(signalR);
+            double[] spectral = Apply_FFT(signalQ);
+
+            Chart newChart = new Chart();
+            newChart.Size = new Size(934, 407);
+            var chartArea = new ChartArea();
+            chartArea.AxisX.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisY.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisX.ScaleView.Zoomable = true;
+            chartArea.CursorX.AutoScroll = true;
+            chartArea.CursorX.IsUserSelectionEnabled = true;
+            newChart.ChartAreas.Add(chartArea);
+
+            var series = new Series();
+            series.Name = "Series1";
+            series.ChartType = SeriesChartType.FastLine;
+            
+            newChart.Series.Add(series);
+            newChart.Series["Series1"].Points.Clear();
+
+            for (int i = 0; i < spectral.Length; i++)
+            {
+                newChart.Series["Series1"].Points.AddY(spectral[i]);
+            }
+            form2.Controls.Add(newChart);
+            form2.Show();
+            this.window += 150;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NAudio.Wave.IWaveProvider provider = new NAudio.Wave.RawSourceWaveStream(new System.IO.MemoryStream(ms.ToArray()), new NAudio.Wave.WaveFormat(sampleRate, 16, 2));
+                waveOut = new NAudio.Wave.DirectSoundOut();
+                waveOut.Init(provider);
+                waveOut.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Please Record Before Playing", "Missing Audio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
